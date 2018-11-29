96f44546eb9417d51bf9b14ba868d02141f5386b
talfreds <tyler.alfreds@gmail.com>
2018-11-28 21:08:50 -0800

    registration and login barebones functionality

---
+++ controllers/load_database.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+
+
+var getDatabase = () => {
+    try {
+        if (fs.existsSync('users_database.json')) {
+
+            var readJSON = fs.readFileSync('users_database.json');
+
+            // read and return the inventory into a JSON
+            var users_database = JSON.parse(readJSON);
+        } else {
+            users_database = [{"login":"", "password":"", "email":""}]
+        }
+
+        return users_database
+
+    } catch (e) {
+        console.log('Error reading file. Please record this error message and contact app developer. \n')
+        console.log(e);
+    }
+}
+
+
+module.exports = { getDatabase }
