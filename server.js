e4f77d039b455e6133173d4cf83b43c97a0a7c76
carson91 <cxu51@my.bcit.ca>
2018-12-04 21:49:10 -0800

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,56 +1,38 @@
 const express = require('express');
+const hbs = require('hbs');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+
 const homeRouter = require('./controllers/home');
 const loginRouter = require('./controllers/login');
 const registerRouter = require('./controllers/register');
 const logOutRouter = require('./controllers/logout');
-const bodyParser = require('body-parser');
 const getMovesRouter = require('./controllers/get_moves');
 
-const hbs = require('hbs');
-var cookieSession = require('cookie-session')
-
 var app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}))
 
 // makes a cookie middleware
 // needs the require cookieSession to work
 app.use(cookieSession({
   name: 'connect4_session',
   keys: [ 'idontknowwhatthisisfor'],
-
-  // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
-
-
-// this might let us use request.body..
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-
-
 
 // directories
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 hbs.registerPartials(__dirname + '/views/partials');
-
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
 
 // route to homepage
 app.use('/', homeRouter);
-
 app.use('/', loginRouter);
-
 app.use('/', registerRouter);
-
 app.use('/', logOutRouter);
 app.use('/', getMovesRouter);
 
